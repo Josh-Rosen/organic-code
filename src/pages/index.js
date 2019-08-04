f71651ddf03c0b135f0f564a729911e5c05c1de9
Josh Rosen <rosenjd4@gmail.com>
2019-08-03 18:06:07 -0700

    style and blog work

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,7 @@
 import React from "react";
 import Layout from './layout';
-import { Link } from "gatsby";
 
 export default () => (
   <Layout>
-    <ul>
-      <li>
-        <Link to="/blog/hello-gatsby" activeClassName=''>
-          Hello Gatsby
-        </Link>
-        <p>Dec 8, 2018</p>
-        <p>Description</p>
-      </li>
-    </ul>
   </Layout>
 );
