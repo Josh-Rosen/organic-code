7221cda8ef54f1ea43223382ca9c0932d109d17f
Josh Rosen <rosenjd4@gmail.com>
2024-03-16 19:55:51 -0600

    update package lock

--- src/App.test.js
+++ src/App.test.js
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-describe('App component tests', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    const root = ReactDOM.createRoot(div);
-    root.render(<App />);
-    root.unmount();
-  });
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  const root = ReactDOM.createRoot(div);
+  root.render(<App />);
+  root.unmount();
 });
